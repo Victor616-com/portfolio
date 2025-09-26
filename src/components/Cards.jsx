c7c91acd5220ace436ab57d857a91d5aedb59f8b
Victor Cretu <eaa24vac@students.eaaa.dk>
2025-09-26 19:01:23 +0200

    Fixed a bug

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -464,7 +464,7 @@ export default function Cards() {
                 textDecoration: "underline"
               }}
             >
-              Read more
+              Read more...
             </p>
           </div>
           
